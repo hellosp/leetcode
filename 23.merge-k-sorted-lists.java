d0aa40aea55a9f195fb0f9c031f764c456b18146
hellosp <hellosp@users.noreply.github.com>
2019-05-09 14:10:35 +0800

    update 23.merge-k-sorted-lists.java

---
+++ 23.merge-k-sorted-lists.java
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=23 lang=java
+ *
+ * [23] Merge k Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-k-sorted-lists/description/
+ *
+ * algorithms
+ * Hard (34.11%)
+ * Total Accepted:    379.4K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+ *
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and
+ * describe its complexity.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * Output: 1->1->2->3->4->4->5->6
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        int interval = 1, length = lists.length;
+        while (interval < length) {
+            int i = 0;
+            while (i + interval < length) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+                i += interval * 2;
+            }
+            interval *= 2;
+        }
+        return lists[0];
+    }
+    
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode header = new ListNode(0);
+        ListNode p = header;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if (l1 != null) {
+            p.next = l1;
+        }
+        if (l2 != null) {
+            p.next = l2;
+        }
+        return header.next;
+    }
+}
