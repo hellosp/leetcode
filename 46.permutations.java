9c5fa056d29e908eaac50db0d7bb79354f5d2659
hellosp <hellosp@users.noreply.github.com>
2019-05-16 20:30:10 +0800

    update 46.permutations.java

---
+++ 46.permutations.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=46 lang=java
+ *
+ * [46] Permutations
+ *
+ * https://leetcode.com/problems/permutations/description/
+ *
+ * algorithms
+ * Medium (54.74%)
+ * Total Accepted:    373.6K
+ * Total Submissions: 680.2K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a collection of distinct integers, return all possible permutations.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3]
+ * Output:
+ * [
+ * ⁠ [1,2,3],
+ * ⁠ [1,3,2],
+ * ⁠ [2,1,3],
+ * ⁠ [2,3,1],
+ * ⁠ [3,1,2],
+ * ⁠ [3,2,1]
+ * ]
+ * 
+ * 
+ */
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        dfs(nums, list);
+        return result;
+    }
+    
+    private void dfs(int[] nums, List<Integer> list) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i])) {
+                continue;
+            }
+            list.add(Integer.valueOf(nums[i]));
+            dfs(nums, list);
+            list.remove(Integer.valueOf(nums[i]));
+        }
+    }
+}
