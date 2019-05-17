2e4159ffa49a3d77f4848ce256443898b5564728
hellosp <hellosp@users.noreply.github.com>
2019-05-17 15:38:29 +0800

    update 50.powx-n.java

---
+++ 50.powx-n.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=50 lang=java
+ *
+ * [50] Pow(x, n)
+ *
+ * https://leetcode.com/problems/powx-n/description/
+ *
+ * algorithms
+ * Medium (27.87%)
+ * Total Accepted:    316.3K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '2.00000\n10'
+ *
+ * Implement pow(x, n), which calculates x raised to the power n (xn).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 2.00000, 10
+ * Output: 1024.00000
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 2.10000, 3
+ * Output: 9.26100
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 2.00000, -2
+ * Output: 0.25000
+ * Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * -100.0 < x < 100.0
+ * n is a 32-bit signed integer, within the range [−231, 231 − 1]
+ * 
+ * 
+ */
+class Solution {
+    public double myPow(double x, int n) {
+        double result = 1;
+        long absN = Math.abs((long)n);
+        while (absN > 0) {
+            if ((absN&1) == 1) {
+                result *= x;
+            }
+            absN >>= 1;
+            x *= x;
+        }
+        return n < 0 ? 1/result : result;
+    }
+}
