754c46e0435fd8a2d2a923b356b2fc8f4a761a74
hellosp <hellosp@users.noreply.github.com>
2019-05-20 18:19:26 +0800

    update 204.count-primes.java

---
+++ 204.count-primes.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=204 lang=java
+ *
+ * [204] Count Primes
+ *
+ * https://leetcode.com/problems/count-primes/description/
+ *
+ * algorithms
+ * Easy (28.91%)
+ * Likes:    1025
+ * Dislikes: 391
+ * Total Accepted:    234K
+ * Total Submissions: 809.5K
+ * Testcase Example:  '10'
+ *
+ * Count the number of prime numbers less than a non-negative number, n.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 10
+ * Output: 4
+ * Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+ * 
+ * 
+ */
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                count++;
+                for (int j = 2; i*j < n; j++) {
+                    notPrime[i*j] = true;
+                }
+            }
+        }
+        return count;
+    }
+}
+
