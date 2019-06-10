53af6dabf89e5700b6a86cc5c720e73e5d170074
hellosp <hellosp@users.noreply.github.com>
2019-06-10 11:19:05 +0800

    update 34.find-first-and-last-position-of-element-in-sorted-array.java

---
+++ 34.find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=34 lang=java
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ *
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (33.42%)
+ * Total Accepted:    295.6K
+ * Total Submissions: 883.2K
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * Given an array of integers nums sorted in ascending order, find the starting
+ * and ending position of a given target value.
+ * 
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * 
+ * If the target is not found in the array, return [-1, -1].
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ * 
+ */
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        
+        int l = 0, r = nums.length - 1;
+        int[] result = new int[2];
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        result[0] = (target == nums[l]) ? l : -1;
+        
+        r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2 + 1;
+            if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid;
+            }
+        }
+        result[1] = (target == nums[r]) ? r : -1;
+        
+        return result;
+    }
+}
