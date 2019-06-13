4f475661de546d67d6df3d0ca9c6753c8f1d27e0
hellosp <hellosp@users.noreply.github.com>
2019-06-13 15:24:44 +0800

    update 31.next-permutation.java

---
+++ 31.next-permutation.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=31 lang=java
+ *
+ * [31] Next Permutation
+ *
+ * https://leetcode.com/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (30.37%)
+ * Total Accepted:    231.9K
+ * Total Submissions: 763.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Implement next permutation, which rearranges numbers into the
+ * lexicographically next greater permutation of numbers.
+ * 
+ * If such arrangement is not possible, it must rearrange it as the lowest
+ * possible order (ie, sorted in ascending order).
+ * 
+ * The replacement must be in-place and use only constant extra memory.
+ * 
+ * Here are some examples. Inputs are in the left-hand column and its
+ * corresponding outputs are in the right-hand column.
+ * 
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ * 
+ */
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) i--;
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) j--;
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+}
