dbb3fda0357fdbf35ace131a7e4bd2e20adebc68
hellosp <hellosp@users.noreply.github.com>
2019-06-14 17:50:15 +0800

    update 227.basic-calculator-ii.java

---
+++ 227.basic-calculator-ii.java
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=227 lang=java
+ *
+ * [227] Basic Calculator II
+ *
+ * https://leetcode.com/problems/basic-calculator-ii/description/
+ *
+ * algorithms
+ * Medium (33.58%)
+ * Likes:    696
+ * Dislikes: 110
+ * Total Accepted:    111.3K
+ * Total Submissions: 331.4K
+ * Testcase Example:  '"3+2*2"'
+ *
+ * Implement a basic calculator to evaluate a simple expression string.
+ * 
+ * The expression string contains only non-negative integers, +, -, *, /
+ * operators and empty spaces  . The integer division should truncate toward
+ * zero.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "3+2*2"
+ * Output: 7
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: " 3/2 "
+ * Output: 1
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: " 3+5 / 2 "
+ * Output: 5
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * You may assume that the given expression is always valid.
+ * Do not use the eval built-in library function.
+ * 
+ * 
+ */
+class Solution {
+    public int calculate(String s) {
+        if(s == null || s.length() == 0) {
+            return 0;
+        }
+        
+        s = s.trim();
+        
+        long prevNum = 0;
+        int sum = 0;
+        char prevOp = '+';
+        
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == ' ') continue;
+            if(Character.isDigit(c)) {
+                int val = c - '0';
+                while(i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    val = val * 10 + (s.charAt(i + 1) - '0');
+                    i++;
+                }
+                if (prevOp == '+') {
+                    sum += prevNum;
+                    prevNum = val;
+                } else if (prevOp == '-') {
+                    sum += prevNum;
+                    prevNum = -val;
+                } else if (prevOp == '*') {
+                    prevNum = prevNum * val;
+                } else if (prevOp == '/') {
+                    prevNum = prevNum/val;
+                }
+            } else {
+                prevOp = c;
+            }
+        }
+        
+        sum += prevNum;
+        return sum;
+    }
+}
+
