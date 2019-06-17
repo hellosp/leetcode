dbbfdac14946c81df160f93f592d915021291ea8
hellosp <hellosp@users.noreply.github.com>
2019-06-17 19:58:42 +0800

    update 10.regular-expression-matching.java

---
+++ 10.regular-expression-matching.java
@@ -0,0 +1,119 @@
+/*
+ * @lc app=leetcode id=10 lang=java
+ *
+ * [10] Regular Expression Matching
+ *
+ * https://leetcode.com/problems/regular-expression-matching/description/
+ *
+ * algorithms
+ * Hard (25.32%)
+ * Likes:    2674
+ * Dislikes: 520
+ * Total Accepted:    309.9K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"aa"\n"a"'
+ *
+ * Given an input string (s) and a pattern (p), implement regular expression
+ * matching with support for '.' and '*'.
+ * 
+ * 
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * 
+ * 
+ * The matching should cover the entire input string (not partial).
+ * 
+ * Note:
+ * 
+ * 
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters
+ * like . or *.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the precedeng element, 'a'.
+ * Therefore, by repeating 'a' once, it becomes "aa".
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input:
+ * s = "aab"
+ * p = "c*a*b"
+ * Output: true
+ * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore
+ * it matches "aab".
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
+ * 
+ * 
+ */
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if(s == null || p == null) {
+            return false;
+        }
+        boolean[][] state = new boolean[s.length() + 1][p.length() + 1];
+        state[0][0] = true;
+        // no need to initialize state[i][0] as false
+        // initialize state[0][j]
+        for (int j = 1; j < state[0].length; j++) {
+            if (p.charAt(j - 1) == '*') {
+                if (state[0][j - 1] || (j > 1 && state[0][j - 2])) {
+                    state[0][j] = true;
+                }
+            } 
+        }
+        for (int i = 1; i < state.length; i++) {
+            for (int j = 1; j < state[0].length; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    state[i][j] = state[i - 1][j - 1];
+                }
+                if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
+                        state[i][j] = state[i][j - 2];
+                    } else {
+                        state[i][j] = state[i - 1][j] || state[i][j - 1] || state[i][j - 2];
+                    }
+                }
+            }
+        }
+        return state[s.length()][p.length()];
+    }
+}
+
