17f8ec908a59bb8524a2e1d2e6c23409dc016267
hellosp <hellosp@users.noreply.github.com>
2019-06-17 14:34:24 +0800

    update 300.longest-increasing-subsequence.java

---
+++ 300.longest-increasing-subsequence.java
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=300 lang=java
+ *
+ * [300] Longest Increasing Subsequence
+ *
+ * https://leetcode.com/problems/longest-increasing-subsequence/description/
+ *
+ * algorithms
+ * Medium (40.88%)
+ * Likes:    2534
+ * Dislikes: 58
+ * Total Accepted:    226.1K
+ * Total Submissions: 552.9K
+ * Testcase Example:  '[10,9,2,5,3,7,101,18]'
+ *
+ * Given an unsorted array of integers, find the length of longest increasing
+ * subsequence.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [10,9,2,5,3,7,101,18]
+ * Output: 4 
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore
+ * the length is 4. 
+ * 
+ * Note: 
+ * 
+ * 
+ * There may be more than one LIS combination, it is only necessary for you to
+ * return the length.
+ * Your algorithm should run in O(n^2) complexity.
+ * 
+ * 
+ * Follow up: Could you improve it to O(n log n) time complexity?
+ * 
+ */
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int len = 0;
+        int[] tails = new int[nums.length];
+
+        tails[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < tails[0]) {
+                tails[0] = nums[i];
+            } else if (nums[i] > tails[len]) {
+                tails[++len] = nums[i];
+            } else {
+                tails[binarySearch(tails, 0, len, nums[i])] = nums[i];
+            }
+        }
+        return len + 1;
+    }
+
+    private int binarySearch(int[] nums, int l, int r, int target) {
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
+
