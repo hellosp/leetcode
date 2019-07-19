fe0091127cdb1fe0c83789c7127837e72d895f94
hellosp <hellosp@users.noreply.github.com>
2019-07-19 10:48:02 +0800

    update 137.single-number-ii.java

--- 137.single-number-ii.java
+++ 137.single-number-ii.java
@@ -37,12 +37,18 @@
  */
 class Solution {
     public int singleNumber(int[] nums) {
-        int low = 0, high = 0;
-        for (int i = 0; i < nums.length; i++) {
-            low = (low ^ nums[i]) & ~high;
-            high = (high ^ nums[i]) & ~low;
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            int mask = 1<<i;
+            for (int j = 0; j < nums.length; j++) {
+                if ((nums[j] & mask) != 0) {
+                    sum ++;
+                }
+            }
+            res |= (sum % 3) << i;
         }
-        return low;
+        return res;
     }
 }
 
