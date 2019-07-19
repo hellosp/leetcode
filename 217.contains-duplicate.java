0274ef5a2e478b73dc6d78410ab52faa6e44f8f9
hellosp <hellosp@users.noreply.github.com>
2019-07-19 16:09:15 +0800

    update 217.contains-duplicate.java

---
+++ 217.contains-duplicate.java
@@ -0,0 +1,53 @@
+import java.util.Set;
+
+/*
+ * @lc app=leetcode id=217 lang=java
+ *
+ * [217] Contains Duplicate
+ *
+ * https://leetcode.com/problems/contains-duplicate/description/
+ *
+ * algorithms
+ * Easy (52.10%)
+ * Likes:    398
+ * Dislikes: 520
+ * Total Accepted:    341.1K
+ * Total Submissions: 654.8K
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * Given an array of integers, find if the array contains any duplicates.
+ * 
+ * Your function should return true if any value appears at least twice in the
+ * array, and it should return false if every element is distinct.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3,1]
+ * Output: true
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,3,4]
+ * Output: false
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ * 
+ */
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (!set.add(n)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
