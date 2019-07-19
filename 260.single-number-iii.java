5335e8381b657e493bf2efed3e8c417d2d7e014f
hellosp <hellosp@users.noreply.github.com>
2019-07-19 11:38:44 +0800

    update 260.single-number-iii.java

---
+++ 260.single-number-iii.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=260 lang=java
+ *
+ * [260] Single Number III
+ *
+ * https://leetcode.com/problems/single-number-iii/description/
+ *
+ * algorithms
+ * Medium (57.15%)
+ * Likes:    906
+ * Dislikes: 79
+ * Total Accepted:    110.3K
+ * Total Submissions: 191.9K
+ * Testcase Example:  '[1,2,1,3,2,5]'
+ *
+ * Given an array of numbers nums, in which exactly two elements appear only
+ * once and all the other elements appear exactly twice. Find the two elements
+ * that appear only once.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:  [1,2,1,3,2,5]
+ * Output: [3,5]
+ * 
+ * Note:
+ * 
+ * 
+ * The order of the result is not important. So in the above example, [5, 3] is
+ * also correct.
+ * Your algorithm should run in linear runtime complexity. Could you implement
+ * it using only constant space complexity?
+ * 
+ */
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum ^= nums[i];
+        }
+        sum &= -sum;
+
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if ((sum & nums[i]) == 0) {
+                result[0] ^= nums[i];
+            } else {
+                result[1] ^= nums[i];
+            }
+        }
+        return result;
+    }
+}
+
