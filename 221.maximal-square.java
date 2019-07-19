f62258bf3a49aaaa00aa5f5836673bd5c43a5868
hellosp <hellosp@users.noreply.github.com>
2019-07-19 16:04:41 +0800

    update 221.maximal-square.java

---
+++ 221.maximal-square.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=221 lang=java
+ *
+ * [221] Maximal Square
+ *
+ * https://leetcode.com/problems/maximal-square/description/
+ *
+ * algorithms
+ * Medium (33.15%)
+ * Likes:    1287
+ * Dislikes: 34
+ * Total Accepted:    131.7K
+ * Total Submissions: 397.3K
+ * Testcase Example:  '[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]'
+ *
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest square
+ * containing only 1's and return its area.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 
+ * 
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ * 
+ * Output: 4
+ * 
+ */
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0) {
+            return 0;
+        }
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+
+        int maxEdge = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    maxEdge = Math.max(maxEdge, dp[i][j]);
+                }
+            }
+        }
+        return maxEdge * maxEdge;
+    }
+}
