36c6af8b429e88518311c60816bb8442c62c50f7
hellosp <hellosp@users.noreply.github.com>
2019-09-03 20:26:02 +0800

    update 73.set-matrix-zeroes.java

--- 73.set-matrix-zeroes.java
+++ 73.set-matrix-zeroes.java
@@ -62,23 +62,44 @@
  */
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int[] row = new int[matrix.length];
-        int[] col = new int[matrix[0].length];
-
+        boolean isZeroRow = false, isZeroCol = false;
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                isZeroRow = true;
+                break;
+            }
+        }
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][0] == 0) {
+                isZeroCol = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
             }
         }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (row[i] == 1 || col[j] == 1) {
-                    matrix[i][j] = 0;
-                }
+        if (isZeroRow) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (isZeroCol) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
