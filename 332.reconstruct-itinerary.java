3892f3a216bde1e163898e0d7e44c2e6a6eb2f2d
hellosp <hellosp@users.noreply.github.com>
2019-09-05 14:10:20 +0800

    update 332.reconstruct-itinerary.java

--- 332.reconstruct-itinerary.java
+++ 332.reconstruct-itinerary.java
@@ -56,14 +56,15 @@ class Solution {
         for (List<String> ticket : tickets) {
             String from = ticket.get(0);
             String to = ticket.get(1);
-
-            if (adj.containsKey(from)) {
-                adj.get(from).add(to);
-            } else {
-                PriorityQueue<String> set = new PriorityQueue<>();
-                set.add(to);
-                adj.put(from, set);
-            }
+            
+            // if (adj.containsKey(from)) {
+            //     adj.get(from).add(to);
+            // } else {
+            //     PriorityQueue<String> set = new PriorityQueue<>();
+            //     set.add(to);
+            //     adj.put(from, set);
+            // }
+            adj.computeIfAbsent(from, k -> new PriorityQueue<>()).add(to);
         }
 
         List<String> result = new ArrayList<>();
