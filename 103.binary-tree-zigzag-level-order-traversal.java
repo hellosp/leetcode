4539305884b54da580d488eaf795242492717623
hellosp <hellosp@users.noreply.github.com>
2019-09-10 20:04:26 +0800

    update 103.binary-tree-zigzag-level-order-traversal.java

---
+++ 103.binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+/*
+ * @lc app=leetcode id=103 lang=java
+ *
+ * [103] Binary Tree Zigzag Level Order Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (43.01%)
+ * Likes:    1155
+ * Dislikes: 68
+ * Total Accepted:    247.9K
+ * Total Submissions: 575.6K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, return the zigzag level order traversal of its nodes'
+ * values. (ie, from left to right, then right to left for the next level and
+ * alternate between).
+ * 
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 
+ * return its zigzag level order traversal as:
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [20,9],
+ * ⁠ [15,7]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean isLeft = true;
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int count = queue.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode node = queue.poll();
+                if (isLeft) {
+                    list.add(node.val);
+                } else {
+                    list.add(0, node.val);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            isLeft = !isLeft;
+            result.add(list);
+        }
+
+        return result;
+    }
+}
+
