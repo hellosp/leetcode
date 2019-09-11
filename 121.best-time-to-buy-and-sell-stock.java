9be585924b905b8f9af01e6810408d3d860cf8fa
hellosp <hellosp@users.noreply.github.com>
2019-09-11 11:30:09 +0800

    update 121.best-time-to-buy-and-sell-stock.java

--- 121.best-time-to-buy-and-sell-stock.java
+++ 121.best-time-to-buy-and-sell-stock.java
@@ -45,7 +45,8 @@ class Solution {
     public int maxProfit(int[] prices) {
         int maxCur = 0, result = 0;
         for (int i = 1; i < prices.length; i++) {
-            maxCur = Math.max(0, maxCur += (prices[i] - prices[i - 1]));
+            int balance = prices[i] - prices[i - 1];
+            maxCur = Math.max(balance, maxCur += balance);
             result = Math.max(maxCur, result);
         }
         return result;
