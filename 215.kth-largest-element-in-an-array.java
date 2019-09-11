376146f57d869aca70228ccebbfeb55abd98fcae
hellosp <hellosp@users.noreply.github.com>
2019-09-11 14:09:16 +0800

    update 215.kth-largest-element-in-an-array.java

---
+++ 215.kth-largest-element-in-an-array.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+/*
+ * @lc app=leetcode id=215 lang=java
+ *
+ * [215] Kth Largest Element in an Array
+ *
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+ *
+ * algorithms
+ * Medium (49.48%)
+ * Likes:    2374
+ * Dislikes: 188
+ * Total Accepted:    430.2K
+ * Total Submissions: 866.3K
+ * Testcase Example:  '[3,2,1,5,6,4]\n2'
+ *
+ * Find the kth largest element in an unsorted array. Note that it is the kth
+ * largest element in the sorted order, not the kth distinct element.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [3,2,1,5,6,4] and k = 2
+ * Output: 5
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [3,2,3,1,2,4,5,5,6] and k = 4
+ * Output: 4
+ * 
+ * Note: 
+ * You may assume k is always valid, 1 ≤ k ≤ array's length.
+ * 
+ */
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        for (int i = 0; i < nums.length; i++) {
+            queue.add(nums[i]);
+        }
+
+        int result = 0;
+        for (int i = 0; i < k; i++) {
+            result = queue.poll();
+        }
+        return result;
+    }
+}
+
