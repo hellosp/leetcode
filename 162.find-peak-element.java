7c91c17a002d1004d0574911a9ea22fcc19c8192
hellosp <hellosp@users.noreply.github.com>
2019-09-18 16:32:41 +0800

    update 162.find-peak-element.java

---
+++ 162.find-peak-element.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=162 lang=java
+ *
+ * [162] Find Peak Element
+ *
+ * https://leetcode.com/problems/find-peak-element/description/
+ *
+ * algorithms
+ * Medium (41.30%)
+ * Likes:    834
+ * Dislikes: 1316
+ * Total Accepted:    241.2K
+ * Total Submissions: 584K
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * A peak element is an element that is greater than its neighbors.
+ * 
+ * Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element
+ * and return its index.
+ * 
+ * The array may contain multiple peaks, in that case return the index to any
+ * one of the peaks is fine.
+ * 
+ * You may imagine that nums[-1] = nums[n] = -∞.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,2,3,1]
+ * Output: 2
+ * Explanation: 3 is a peak element and your function should return the index
+ * number 2.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1,2,1,3,5,6,4]
+ * Output: 1 or 5 
+ * Explanation: Your function can return either index number 1 where the peak
+ * element is 2, 
+ * or index number 5 where the peak element is 6.
+ * 
+ * 
+ * Note:
+ * 
+ * Your solution should be in logarithmic complexity.
+ * 
+ */
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (right - left > 1) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (left == nums.length - 1 || nums[left] > nums[left + 1]) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+}
+
