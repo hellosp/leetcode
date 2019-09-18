09ee603f8ca9746c50117364bc39ad0e94280291
hellosp <hellosp@users.noreply.github.com>
2019-09-18 19:50:45 +0800

    update 174.dungeon-game.java

---
+++ 174.dungeon-game.java
@@ -0,0 +1,109 @@
+/*
+ * @lc app=leetcode id=174 lang=java
+ *
+ * [174] Dungeon Game
+ *
+ * https://leetcode.com/problems/dungeon-game/description/
+ *
+ * algorithms
+ * Hard (27.83%)
+ * Likes:    817
+ * Dislikes: 19
+ * Total Accepted:    71.6K
+ * Total Submissions: 255.8K
+ * Testcase Example:  '[[-2,-3,3],[-5,-10,1],[10,30,-5]]'
+ *
+ * table.dungeon, .dungeon th, .dungeon td {
+ * ⁠ border:3px solid black;
+ * }
+ * 
+ * ⁠.dungeon th, .dungeon td {
+ * ⁠   text-align: center;
+ * ⁠   height: 70px;
+ * ⁠   width: 70px;
+ * }
+ * 
+ * The demons had captured the princess (P) and imprisoned her in the
+ * bottom-right corner of a dungeon. The dungeon consists of M x N rooms laid
+ * out in a 2D grid. Our valiant knight (K) was initially positioned in the
+ * top-left room and must fight his way through the dungeon to rescue the
+ * princess.
+ * 
+ * The knight has an initial health point represented by a positive integer. If
+ * at any point his health point drops to 0 or below, he dies immediately.
+ * 
+ * Some of the rooms are guarded by demons, so the knight loses health
+ * (negative integers) upon entering these rooms; other rooms are either empty
+ * (0's) or contain magic orbs that increase the knight's health (positive
+ * integers).
+ * 
+ * In order to reach the princess as quickly as possible, the knight decides to
+ * move only rightward or downward in each step.
+ * 
+ * 
+ * 
+ * Write a function to determine the knight's minimum initial health so that he
+ * is able to rescue the princess.
+ * 
+ * For example, given the dungeon below, the initial health of the knight must
+ * be at least 7 if he follows the optimal path RIGHT-> RIGHT -> DOWN ->
+ * DOWN.
+ * 
+ * 
+ * 
+ * 
+ * -2 (K)
+ * -3
+ * 3
+ * 
+ * 
+ * -5
+ * -10
+ * 1
+ * 
+ * 
+ * 10
+ * 30
+ * -5 (P)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * The knight's health has no upper bound.
+ * Any room can contain threats or power-ups, even the first room the knight
+ * enters and the bottom-right room where the princess is imprisoned.
+ * 
+ * 
+ */
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length, n = dungeon[0].length;
+        int[][] S = new int[m][n];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int right = Integer.MAX_VALUE, down = Integer.MAX_VALUE;
+                if (i < m - 1) {
+                    down = S[i + 1][j];
+                }
+                if (j < n - 1) {
+                    right = S[i][j + 1];
+                }
+                int neighbour = right < down ? right : down;
+                if (i == m - 1 && j == n - 1) {
+                    neighbour = 1;
+                }
+                S[i][j] = neighbour - dungeon[i][j];
+                if (S[i][j] < 1) {
+                    S[i][j] = 1;
+                }
+            }
+        }
+        return S[0][0];
+    }
+}
+
