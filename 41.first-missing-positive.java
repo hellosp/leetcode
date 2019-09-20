3b5e5704cda2d9645c73e29612fa0382ef34fe1d
hellosp <hellosp@users.noreply.github.com>
2019-09-20 11:52:22 +0800

    update 41.first-missing-positive.java

---
+++ 41.first-missing-positive.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=41 lang=java
+ *
+ * [41] First Missing Positive
+ *
+ * https://leetcode.com/problems/first-missing-positive/description/
+ *
+ * algorithms
+ * Hard (29.59%)
+ * Likes:    2066
+ * Dislikes: 645
+ * Total Accepted:    242.5K
+ * Total Submissions: 815.6K
+ * Testcase Example:  '[1,2,0]'
+ *
+ * Given an unsorted integer array, find the smallest missing positive
+ * integer.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,0]
+ * Output: 3
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [3,4,-1,1]
+ * Output: 2
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [7,8,9,11,12]
+ * Output: 1
+ * 
+ * 
+ * Note:
+ * 
+ * Your algorithm should run in O(n) time and uses constant extra space.
+ * 
+ */
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
+
